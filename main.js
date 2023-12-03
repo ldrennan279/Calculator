11f440301ec255b2f74860c392dd56ce46173a73
ldrennan279 <leedrennan279@gmail.com>
2023-12-03 15:00:37 +0000

    commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const main = document.getElementById("display")
+let numberArr = [12, 12]
+const totalResult = ``
+let savedNumber = "24"
+
+function currentNumber(){
+    document.addEventListener("click", ()=>{
+
+    })
+}
+
+function add(){
+
+}
+
+main.innerHTML += `
+    <div class="resultDisplay">
+        <p>${numberArr}</p>
+        <p>${savedNumber}</p>
+    <div>
+`
