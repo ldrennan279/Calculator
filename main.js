9651808a28dec5d7d076196808ddc938245b1b36
ldrennan279 <leedrennan279@gmail.com>
2023-12-10 15:52:00 +0000

    JS update

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-const main = document.getElementById("display")
-let savedNumber = ''
+const display = document.getElementById("savedNumber")
+let displayedNumber = ''
+let newTotalNumber = ''
 
 document.addEventListener("click", (e)=>{
     if (e.target.className === 'number'){
         let btn = e.target.value
-        savedNumber += btn
-        }
-    // } else if(e.target.className === 'operator'){
+        displayedNumber += btn
+        updateDisplay()
 
+
+        } else if(e.target.className === 'operator'){
+            let btn = e.target.value
+            console.log(btn)
+            newTotalNumber = displayedNumber
+            
+        }
     // } else if(e.target.className === 'equals'){
 
     // } else if(e.target.className === 'delete')
-    
+
 
 })
 
 
-// main.innerHTML += `
-//     <div class="resultDisplay">
-//         <p>0</p>
-//     <div>
-// `
+function updateDisplay(){
+    display.innerHTML = `
+            <p>${displayedNumber}</p>
+
+`
+}
+
+
